1a8983a5e94eee4875ba7c43a4a28eeb89c5223e
patgarcia <commonzenmedia@gmail.com>
2021-04-13 01:06:36 -0400

    Alien Block Logic Nailed 🦾

--- static/js/physics_engine.js
+++ static/js/physics_engine.js
@@ -45,9 +45,11 @@ for(sound in sounds){
     sounds[sound] = soundNode;
 }
 
-// Entity Model
+/*============= 
+  ENTITY MODEL
+ =============*/
 class Entity {
-    constructor(parentElem, className) {
+    constructor(parentElem) {
         this.x;
         this.y;
         this.cx;
@@ -56,11 +58,10 @@ class Entity {
         this.height;
         this.domElem;
         this.sprite;
-        this.className = className;
-        this.parentElem = parentElem;
-        this.shotBy;
+        this.className = this.constructor.name.toLowerCase();
+        this.parentElem = parentElem || null;
 
-        this.domInit();
+        if(parentElem) this.domInit();
     }
     get boundRect() {
         return this.domElem.getBoundingClientRect();
@@ -76,9 +77,13 @@ class Entity {
     }
 
     get center() {
-        this.updateCoordsAndDimensions();
+        this.calcHalvesAndCenters();
         let [cx, cy] = [this.cx, this.cy];
         return { cx, cy }
+    }
+
+    static getClassName() {
+        return this.constructor.name
     }
 }
 
@@ -95,59 +100,32 @@ Entity.prototype.domInit = function () {
     this.parentElem.appendChild(this.domElem);
 
     // get DOM computed coordinates and dimensions
-    this.updateCoordsAndDimensions();
-};
+    this.locationFromBoundingBoxes();
 
-Entity.prototype.followScroller = function(normalizedPosition){
-    let { width: parentWidth } = this.parentBoundRect;
-    this.x = normalizedPosition * (parentWidth - this.width);
-    this.domElem.style.transform = `translate(${this.x}px)`;
+    // Pre-compute Half width and height as well as cx and cy
+    this.calcHalvesAndCenters();
 }
 
-Entity.prototype.itemLocation = function(){
-    this.domElem.style.transform = `translate(${this.x}px, ${this.y}px)`
-}
-
-Entity.prototype.shootLaser = function(){
-    if(lasers.length !== 0) return
-    const laser = new Entity(playArea, 'laser');
-    laser.domElem.classList.add('hide');
-    laser.shotBy = this;
-    laser.direction = this.className == 'spaceship' ? -1 : 1;;
-    laser.x = this.center.cx;
-    laser.y = this.y;
-    laser.itemLocation();
-    lasers.push(laser);
-    sounds.shoot.cloneNode().play();
-    laser.domElem.classList.remove('hide');
-}
-
-Entity.prototype.removeLaser = function(){
-    this.domElem.remove();
-    lasers.pop();
-    delete(this); 
-}
-
-Entity.prototype.alienHit = function(){
-    sounds.alienHit.cloneNode().play();
-    this.domElem.classList.remove(this.className)
-    this.domElem.classList.add('explosion')
-    this.sprite.classList.add('explosion_sprite')
-    this.sprite.classList.remove(this.className + "_sprite")
-    this.domElem.style.opacity = 0;
-
-}
-
-// using DOM getBoundClientRect update Entity's props
-Entity.prototype.updateCoordsAndDimensions = function () {
+Entity.prototype.locationFromBoundingBoxes = function(){
+    // instantiate object with bounding boxes
     let { x: parentX, y: parentY } = this.parentBoundRect;
     let { x, y, width, height } = this.boundRect;
     [this.x, this.y, this.width, this.height] = [x - parentX, y - parentY, width, height];
+}
+
+
+Entity.prototype.calcHalvesAndCenters = function () {
     // center logic is not considering the parent object displacement
     [this.halfWidth, this.halfHeight] = [this.width / 2, this.height / 2]
     this.cx = Math.abs(this.x + this.halfWidth);
     this.cy = Math.abs(this.y + this.halfHeight);
 };
+
+// Check Y-axis overlap, no need to check for collision if none
+// This uses the alien-block y val to avoid computing per object
+Entity.prototype.overlapInY = function(minima){
+    return this.y < minima
+}
 
 // Colission detection logic | Ignores entry side
 Entity.prototype.detectColission = function (other) {
@@ -158,8 +136,10 @@ Entity.prototype.detectColission = function (other) {
               |-----|   +   |------|  withBetweenCXs
         
     */
-    this.updateCoordsAndDimensions();
-    other.updateCoordsAndDimensions();
+
+    this.calcHalvesAndCenters();
+    other.locationFromBoundingBoxes();
+    other.calcHalvesAndCenters();
 
     let dxBetweenEntitiesCXs = Math.abs(this.cx - other.cx);
     let withBetweenCXs = this.halfWidth + other.halfWidth;
@@ -172,25 +152,151 @@ Entity.prototype.detectColission = function (other) {
     return colissionX && colissionY
 }
 
+Entity.prototype.itemLocation = function(){
+    this.domElem.style.transform = `translate(${this.x}px, ${this.y}px)`
+}
+
+/*========== 
+  SPACESHIP
+ ==========*/
+class Spaceship extends Entity{
+
+}
+
+// Ship controller
+Spaceship.prototype.followScroller = function(normalizedPosition){
+    let { width: parentWidth } = this.parentBoundRect;
+    this.x = normalizedPosition * (parentWidth - this.width);
+    this.domElem.style.transform = `translate(${this.x}px)`;
+}
+
+// Ship shooting
+Spaceship.prototype.shootLaser = function(){
+    if(lasers.length !== 0) return
+    sounds.shoot.cloneNode().play();
+    const laser = new Laser (playArea, 'laser');
+    laser.domElem.classList.add('hide');
+    laser.shotBy = this;
+    laser.direction = this.className == 'spaceship' ? -1 : 1;
+    this.locationFromBoundingBoxes();
+    this.calcHalvesAndCenters();
+    laser.x = this.center.cx;
+    laser.y = this.y;
+    laser.itemLocation();
+    lasers.push(laser);
+    laser.domElem.classList.remove('hide');
+}
+
+/*====== 
+  LASER
+ ======*/
+class Laser extends Entity{
+
+}
+class AlienLaser extends Laser{
+
+}
+
+// Laser Extended Detect Collision
+Laser.prototype.detectColission = function(other){
+    // Get the y + height value of the alienContainer
+    // Use that to filter laser detectColission if laser.y >
+    return Entity.prototype.detectColission.call(this, other);
+}
+
+// Laser Removal
+Laser.prototype.removeLaser = function(){
+    this.domElem.remove();
+    lasers.pop();
+    delete(this); 
+}
+
+/*====== 
+  ALIEN
+ ======*/
+
+alienContainer = document.getElementById('alien-container');
+
+function getAlienBlocksMinima(){
+    
+}
+class Alien extends Entity{
+    constructor(){
+        super();
+        this.parentElemName = this.className + '-block';
+        let parentElem = document.getElementById(this.parentElemName);
+        if(!parentElem){
+            let parent = document.createElement('div');
+            parent.id = this.parentElemName;
+            alienContainer.appendChild(parent);
+            this.parentElem = parent;
+        }
+        else{
+            this.parentElem = parentElem;
+        }
+        this.domInit();
+    }
+}
+
+// For Alien only
+Alien.prototype.alienHit = function(){
+    sounds.alienHit.cloneNode().play();
+    this.domElem.classList.remove(this.className)
+    this.domElem.classList.add('explosion')
+    this.sprite.classList.add('explosion_sprite')
+    this.sprite.classList.remove(this.className + "_sprite")
+    this.domElem.style.opacity = 0;
+    this.domElem.style.width = `${this.width}px`; // keep same distance between aliens
+    this.domElem.style.height = `${this.height}px`;
+    let hitIndex = aliens.indexOf(this);
+    aliens = aliens.slice(0,hitIndex++).concat(aliens.slice(hitIndex)) // update array
+}
+
+// Alien types
+class Octopus extends Alien {}
+class Crab extends Alien {}
+class Squid extends Alien {}
+
+
+/*========== 
+  DOM SETUP
+ ==========*/
+
 // DOM elements
 header = document.querySelector('header')
-alienBlock = document.getElementById('alien-block');
 
-// spaceCraft and Alien
-let spaceCraft = new Entity(playArea, 'spaceship');
+// spaceCraft instantiation
+let spaceCraft = new Spaceship(playArea);
 spaceCraft.domElem.style.display = 'none';
 spaceCraft.domElem.classList.add('spaceship_position')
 spaceCraft.y = spaceCraft.parentBoundRect.y + spaceCraft.parentBoundRect.height;
 spaceCraft.domElem.removeAttribute('style');
 
-const aliens = [];
-for (let i = 0; i < 10; i++) {
-    let alien = new Entity(alienBlock, 'alien');
-    aliens.push(alien);
+// Alien Container
+let aliens = [];
+
+// Alien instantiation
+function createAlienRow(AlienType){
+    for (let i = 0; i < 11; i++) {
+        let alien = new AlienType();
+        aliens.push(alien);
+    }
+}
+
+[Alien, Octopus, Crab, Squid].reverse().forEach( a => createAlienRow(a))
+
+// Get Alien Blocks
+const alienBlocks = Array.from(new Set(aliens.map(x => x.parentElem)));
+function getAlienBlocksMinima() {
+    return alienBlocks
+        .map(b => b.getBoundingClientRect())
+        .map(({ y, height }) => Number(y) + Number(height))
+        .map((minima, i) => ({ [alienBlocks[i].id]: minima }))
+        .reduce((acum, currentVal) => Object.assign(acum, currentVal), {})
 }
 
 // shooting event listener
-playArea.addEventListener('click', ()=> spaceCraft.shootLaser())
+playArea.addEventListener('click', () => spaceCraft.shootLaser())
 
 // Stretch the window Hack
 let longElemHeight = 10000;
@@ -209,7 +315,6 @@ const motionRange = bottomLimit - topLimit;
 
 function scrollToLimit() {
     if (scrollY < topLimit) {
-        console.log(scrollY, scrollY < topLimit, 'moving to top')
         scrollTo(0, topLimit + 1)
     }
     else if (scrollY > bottomLimit) {
@@ -220,14 +325,16 @@ function scrollToLimit() {
     spaceCraft.followScroller(normalizedPosition);
 }
 
+let laserSpeed = 40;
 function lasersPositioning() {
     if(lasers.length){
         const laser = lasers[0];
-        laser.y += (120 * laser.direction)
-        if(laser.y  > `-${header.offsetHeight}` ){
+        laser.y += (laserSpeed * laser.direction)
+        if(laser.y  > `-${header.offsetHeight + laserSpeed}` ){
             laser.itemLocation();
         }
         else{
+
             laser.domElem.remove();
             lasers.pop()
             delete(laser);            
@@ -235,13 +342,29 @@ function lasersPositioning() {
     }
 }
 
-// collision resolution
-// Colission Illustration, logic to run in gameloop
-function colissionResolution() {
-    for (const alien of aliens) {
-        hitLaser = lasers.length ? lasers[0].detectColission(alien) : false
-        if (spaceCraft.detectColission(alien) || hitLaser) {
 
+/*============ 
+  GAME ENGINE
+ ============*/
+
+// Collision resolution
+function colissionResolution() {
+    const BlocksMinima = getAlienBlocksMinima();
+    const absMinima = Math.min(...Object.values(BlocksMinima));
+    const shipInRange = spaceCraft.overlapInY(absMinima);
+    const laserInRange = lasers.length ? lasers[0].overlapInY(absMinima) : false;
+    if(!shipInRange && !laserInRange) return
+
+    for (const alien of aliens.slice().reverse()) {
+        const blockMinima = BlocksMinima[alien.parentElem.id]
+        const shipInRange = spaceCraft.overlapInY(blockMinima);
+        const laserInRange = lasers.length ? lasers[0].overlapInY(blockMinima) : false;
+    
+
+        hitLaser = laserInRange ? lasers[0].detectColission(alien) : false;
+        hitCraft = shipInRange ? spaceCraft.detectColission(alien) : false;
+
+        if (hitCraft || hitLaser) {
             alien.alienHit();
             if (hitLaser) lasers[0].removeLaser();
         }
@@ -265,11 +388,16 @@ function gameLoop() {
             colissionResolution();
             gameLoop();
 
-        }, 20);
+        }, 23);
     }
 }
 
 gameLoop();
+
+
+/*============= 
+  HACKIE STUFF
+ =============*/
 
 // set the scroller to the middle of the page hack
 // browser remembers the scrolling position and goes to it
